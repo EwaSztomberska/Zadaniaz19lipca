f351f3afd0bd0ca332a164327bb10250cfc2ea70
EwaSztomberska <ewa.sztomberska@com>
2020-07-19 12:58:17 +0200

    Zadanie 14Runner

---
+++ src/com/company/Zadanie14Runner.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Zadanie14Runner {
+    public static void main(String[] args) {
+        Zadanie14 zadanie14 = new Zadanie14();
+        Zadanie14.Chars aChar = zadanie14.getChar();
+        System.out.println(zadanie14.getResult(aChar));
+    }
+}
