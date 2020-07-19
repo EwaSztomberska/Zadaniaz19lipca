15640c27ef061387efb9b2223aabe214f9aac632
EwaSztomberska <ewa.sztomberska@com>
2020-07-19 12:18:09 +0200

    Zadanie 13Runner

---
+++ src/com/company/Zadanie13Runner.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Zadanie13Runner {
+    public static void main(String[] args) {
+        Zadanie13 zadanie13 = new Zadanie13();
+        zadanie13.run();
+    }
+}
